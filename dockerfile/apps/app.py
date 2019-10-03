1a9644b58f3641b28e71960a822586a5422be4a7
Isaac Faber <isaacfab@gmail.com>
2019-10-02 23:46:32 -0400

    adding full example with dockerfile

--- dockerfile/apps/app.py
+++ dockerfile/apps/app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import joblib
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return "try the predict route it is great!"
 
+@app.route('/predict')
+def predict():
+	 #use entries from the query string here but could also use json
+     age = request.args.get('age')
+     absences = request.args.get('absences')
+     health = request.args.get('health')
+     data = [[age],[health],[absences]]
+     query_df = pd.DataFrame({ 'age' : pd.Series(age) ,'health' : pd.Series(health) ,'absences' : pd.Series(absences)})
+     query = pd.get_dummies(query_df)
+     prediction = clf.predict(query)
+     return jsonify(np.asscalar(prediction))
 
 if __name__ == '__main__':
+    clf = joblib.load('/apps/model.pkl')
     app.run(host="0.0.0.0", debug=True)
